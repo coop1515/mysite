2c3db0fc2d3b04b7d3a536f64670e3a75b9bf95a
김현석 <gustjr324@gmail.com>
2022-05-25 12:15:35 +0900

    main controller #final

--- mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -1,36 +1,33 @@
 package com.douzone.mysite.controller;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.douzone.mysite.web.mvc.main.MainActionFactory;
+import com.douzone.mysite.web.mvc.user.UserActionFactory;
 import com.douzone.web.mvc.Action;
 import com.douzone.web.mvc.ActionFactory;
 
-
-public class MainController extends HttpServlet {
+public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		
+
 //		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/main/index.jsp");
 //		rd.forward(request, response);
 		String actionName = request.getParameter("a");
-		ActionFactory factory = new MainActionFactory();
+		ActionFactory factory = new UserActionFactory();
 		Action action = factory.getAction(actionName);
 		action.execute(request, response);
-		
 	}
 
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
---
+++ mysite02/src/main/java/com/douzone/mysite/web/mvc/user/UserActionFactory.java
@@ -0,0 +1,24 @@
+package com.douzone.mysite.web.mvc.user;
+
+import com.douzone.mysite.web.mvc.main.DefaultAction;
+import com.douzone.web.mvc.Action;
+import com.douzone.web.mvc.ActionFactory;
+
+public class UserActionFactory extends ActionFactory {
+
+	@Override
+	public Action getAction(String actionName) {
+		Action action = null;
+		
+		
+		if("".equals(actionName)) {
+//			action = new JoinAction();
+		}else if("".equals(actionName)) {
+//			action 
+		} else {
+			action = new DefaultAction();
+		}
+		return action;
+	}
+
+}
