c8fcb5a81f5eb002926049f642c92613f95e79a3
김현석 <gustjr324@gmail.com>
2022-06-13 12:20:00 +0900

    AuthInterceptor + @Annotation

--- mysite03/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.douzone.mysite.security.Auth;
 import com.douzone.mysite.service.UserService;
 import com.douzone.mysite.vo.UserVo;
 
@@ -39,30 +40,39 @@ public class UserController {
 		return "user/login";
 	}
 	
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String login(HttpSession session, UserVo vo, Model model) {
-//		System.out.println(vo);
-		
-		UserVo authUser = userService.getUser(vo);
-//		System.out.println(authUser);
-		if(authUser == null) {
-			model.addAttribute("result", "fail");
-			model.addAttribute("email", vo.getEmail());
-			return "user/login";
-		}
-		// 인증처리
-		session.setAttribute("authUser", authUser);
-//		return "redirect:/user/main";
-		return "redirect:/";
+//	@RequestMapping(value = "/login", method = RequestMethod.POST)
+//	public String login(HttpSession session, UserVo vo, Model model) {
+////		System.out.println(vo);
+//		
+//		UserVo authUser = userService.getUser(vo);
+////		System.out.println(authUser);
+//		if(authUser == null) {
+//			model.addAttribute("result", "fail");
+//			model.addAttribute("email", vo.getEmail());
+//			return "user/login";
+//		}
+//		// 인증처리
+//		session.setAttribute("authUser", authUser);
+////		return "redirect:/user/main";
+//		return "redirect:/";
+//	}
+	
+	@RequestMapping(value = "/auth")
+	public void auth() {
 	}
+	
+//	@RequestMapping("/logout")
+//	public String logout(HttpSession session) {
+//		session.removeAttribute("authUser");
+//		session.invalidate();
+//		return "redirect:/";
+//	}
 	
 	@RequestMapping("/logout")
-	public String logout(HttpSession session) {
-		session.removeAttribute("authUser");
-		session.invalidate();
-		return "redirect:/";
+	public void logout() {
 	}
 	
+	@Auth
 	@RequestMapping(value = "/update", method = RequestMethod.GET)
 	public String update(HttpSession session, Model model) {
 		// 접근 제어(Access Control)
