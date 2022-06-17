fe5672026d0f750081470bab87f443791eeb1a53
김현석 <gustjr324@gmail.com>
2022-06-17 12:13:24 +0900

    mysite05 - ServletContainerInitializer

---
+++ mysite05/src/main/java/com/douzone/mysite/initializer/MySiteWebApplicationInitializer.java
@@ -0,0 +1,47 @@
+package com.douzone.mysite.initializer;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.FrameworkServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.douzone.mysite.config.app.AppConfig;
+import com.douzone.mysite.config.web.WebConfig;
+
+public class MySiteWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[] {AppConfig.class};
+//		int[] a = new int[] {1,2,3,4,5}; 이것과 같은거임.
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		
+		return new Class<?>[] {WebConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+	
+		return new String[] {"/"};
+	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		
+		return new Filter[] {new CharacterEncodingFilter("utf-8")};
+	}
+
+	@Override
+	protected FrameworkServlet createDispatcherServlet(WebApplicationContext servletAppContext) {
+		DispatcherServlet servlet = (DispatcherServlet)super.createDispatcherServlet(servletAppContext);
+		servlet.setThrowExceptionIfNoHandlerFound(true);
+		return servlet;
+	}
+	
+}
