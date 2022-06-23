af11db75355d7ff52dfae97e47264f96acc21df3
김현석 <gustjr324@gmail.com>
2022-06-23 12:18:00 +0900

    mysite06 #3

--- mysite06/src/main/java/com/douzone/mysite/config/WebConfig.java
+++ mysite06/src/main/java/com/douzone/mysite/config/WebConfig.java
@@ -1,15 +1,22 @@
 package com.douzone.mysite.config;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.douzone.mysite.security.AuthInterceptor;
+import com.douzone.mysite.security.AuthUserHandlerMethodArgumentResolver;
+import com.douzone.mysite.security.LoginInterceptor;
+import com.douzone.mysite.security.LogoutInterceptor;
 import com.douzone.mysite.security.SiteInterceptor;
 
 @SpringBootConfiguration
@@ -25,17 +32,60 @@ public class WebConfig implements WebMvcConfigurer {
 			return new SiteInterceptor();
 		}
 		
+	// securityIntercepter
+		@Bean
+		public HandlerInterceptor loginInterceptor() {
+			return new LoginInterceptor();
+		}
+		
+		@Bean
+		public HandlerInterceptor logoutInterceptor() {
+			return new LogoutInterceptor();
+		}
+		
+		@Bean
+		public HandlerInterceptor authInterceptor() {
+			return new AuthInterceptor();
+		}
+		
+		// Argument Resolver
+		@Bean
+		public HandlerMethodArgumentResolver handlerMethodArgumentResolver() {
+			return new AuthUserHandlerMethodArgumentResolver();
+		}
+		
+		
 		@Override
 		public void addInterceptors(InterceptorRegistry registry) {
+			// SiteIntercepter
 			registry.addInterceptor(handlerMethod()).addPathPatterns("/**")
+			.excludePathPatterns("/assets/**")
+			.excludePathPatterns("/user/auth")
+			.excludePathPatterns("/user/logout");
+			
+			// securityIntercepter
+			registry.addInterceptor(loginInterceptor()).addPathPatterns("/user/auth");
+			
+			registry.addInterceptor(logoutInterceptor()).addPathPatterns("/user/logout");
+			
+			registry.addInterceptor(authInterceptor()).addPathPatterns("/**")
 			.excludePathPatterns("/assets/**")
 			.excludePathPatterns("/user/auth")
 			.excludePathPatterns("/user/logout");
 		}
 		
 		@Override
+		public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+			resolvers.add(handlerMethodArgumentResolver());
+			}
+		
+		@Override
 		public void addResourceHandlers(ResourceHandlerRegistry registry) {
 			registry.addResourceHandler(env.getProperty("fileupload.resourceMapping"))
 			.addResourceLocations("file:" + env.getProperty("fileupload.uploadLocation"));
-			}
+			
+			registry.addResourceHandler("/assets/**")
+			.addResourceLocations("classpath:/static/");
+		
+		}
 }
